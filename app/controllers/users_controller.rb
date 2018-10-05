23cf071536f4202c3adb707ddcd6830fd751d25a
drillkun <koji729@i.softbank.jp>
2018-10-05 14:47:00 +0900

    thrird_day

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit,:update]}
   
   def index
     @users = User.all
@@ -16,9 +19,11 @@ class UsersController < ApplicationController
     @user = User.new(
     name: params[:name],
     email: params[:email],
+    password: params[:password],
     image_name: "IMG_0750.JPG"
     )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -55,6 +60,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
@@ -62,6 +68,19 @@ class UsersController < ApplicationController
       @email = params[:email]
       @password = params[:password]
       render("users/login_form")
+    end
+  end
+  
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+  
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] ="権限がありません"
+      redirect_to("/posts/index")
     end
   end
   
